8fb38dd663b58933b995569b5d465f6609bc71e6
Arief <ariefmuhammad08@gmail.com>
2022-12-15 10:55:04 +0700

    update

--- final-project-local/dags/sql/fact_total_state.sql
+++ final-project-local/dags/sql/fact_total_state.sql
@@ -1,13 +1,14 @@
-create table if not exists fact_total_city (
+create table if not exists fact_city_per_state (
 	id serial primary key,
+	state varchar,
 	total_city int not null,
 	timestamp timestamp not null
 );
 
-insert into fact_total_city (total_city, timestamp)
-	select count(office_city),current_timestamp from 
-		(select c.office_city from companies c 
-			group by c.office_city) as total 
+insert into fact_city_per_state (total_city, timestamp)
+	select count(office_city), office_state_code ,current_timestamp from companies c
+		group by c.office_state_code 
+		
 
 create table if not exists fact_office_per_state (
 	id serial primary key,
